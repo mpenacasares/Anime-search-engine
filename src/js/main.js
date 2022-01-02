e35046d239532e176d710b347d1ec0ce453fd171
mpenacasares <m.penacasares@gmail.com>
2022-01-02 20:25:01 +0100

    Add function to search and paint

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,45 @@
+// eslint-disable-next-line quotes
 "use strict";
+
+// HTML
+const inputSearch = document.querySelector(".js_input");
+const btnSearch = document.querySelector(".js_btnSearch");
+const btnReset = document.querySelector(".js_btnReset");
+const animeResults = document.querySelector(".js_animeResults");
 
 // server request
 
 let data = [];
-
-const getDataApi = () => {
-  fetch("https://api.jikan.moe/v3/search/anime?q=naruto)")
+function getAnimeResult() {
+const userValue = inputSearch.value;
+// quitar el limit de resultados
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${userValue}&limit=4`)
     .then((response) => response.json())
     .then((dataApi) => {
       data = dataApi.results;
       console.log(data);
+      renderAnimeResult();
     });
-};
+}
 
-getDataApi();
+// paint images
+function renderAnimeResult() {
+  animeResults.innerHTML = "";
+  for (const eachAnime of data) {
+    animeResults.innerHTML += `<li class="js_animeList">
+        <img src=${eachAnime.image_url} alt="Image of ${eachAnime.title}"><h3 class="js_animeTitle">${eachAnime.title}</h3>
+        </li>`;
+  }
+}
+
+function handleSearch(ev){
+ev.preventDefault();
+if (inputSearch.value!==""){
+getAnimeResult();
+}else{
+    animeResults.innerHTML="<p>Introduce primero una serie :)</p>"
+}
+
+}
+
+btnSearch.addEventListener("click",handleSearch)
