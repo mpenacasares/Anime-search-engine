7f7ba2c1583214e1516ede734ac548741dc4b3d5
mpenacasares <m.penacasares@gmail.com>
2022-01-02 14:15:56 +0100

    Add server request

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-'use strict';
+"use strict";
 
+// server request
 
+let data = [];
+
+const getDataApi = () => {
+  fetch("https://api.jikan.moe/v3/search/anime?q=naruto)")
+    .then((response) => response.json())
+    .then((dataApi) => {
+      data = dataApi.results;
+      console.log(data);
+    });
+};
+
+getDataApi();
